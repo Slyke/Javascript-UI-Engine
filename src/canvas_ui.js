3116df82b1fd38644b1c9916d9563483c805d581
Steven Lawler <slawler@architech.ca>
2016-05-11 11:32:00 -0400

    Added URL to the canvas UI file

--- src/canvas_ui.js
+++ src/canvas_ui.js
@@ -2,6 +2,7 @@
   By: Steven Lawler (Slyke)
   Email: steven.lawler777@gmail.com
   Creation Date: 21/09/2014
+  URL: https://github.com/Slyke/Javascript-UI-Engine
   Version: 1.5a
   Description:
     This is a simple canvas control class for Javascript. This class can be used as an instantiated object or as a singleton.
